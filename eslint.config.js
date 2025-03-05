ffd57b7ecd2bcd8a368d3a321cb5bc55a7cc8e4e
Mackie Underdown <macklinu@users.noreply.github.com>
2025-03-05 10:20:56 -0500

    Fix import

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
 import config from '@macklinu/eslint-config'
-import vitest from '@macklinu/eslint-config/vitest.js'
+import vitest from '@macklinu/eslint-config/vitest'
 
 export default [...config, ...vitest]

1c964faba82556c9909b775eeb801f3a694e0719
Mackie Underdown <macklinu@users.noreply.github.com>
2026-08-28 08:33:06 -0400

    build: bundle CLI with tsdown

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
-import { createRequire } from 'node:module'
+import packageManifest from '../package.json' with { type: 'json' }
 
 import { NodeRuntime, NodeServices } from '@effect/platform-node'
 import { Effect } from 'effect'
 import { Command } from 'effect/unstable/cli'
 
 import { setup } from './program.js'
-
-const require = createRequire(import.meta.url)
-const packageManifest = require('../package.json') as { readonly version: string }
 
 Command.run(setup, { version: packageManifest.version }).pipe(
   Effect.provide(NodeServices.layer),
